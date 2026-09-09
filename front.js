a4d47a821550fe08d08f4b16332da30771d056c7
neko-TON <ivantaobao145@gmail.com>
2026-09-09 11:47:43 +0300

    Match DNA tilt to reference

--- front.js
+++ front.js
@@ -168,7 +168,7 @@
       if (ticking) return;
       ticking = true;
       requestAnimationFrame(function () {
-        ghost.style.transform = "translateY(" + (window.scrollY * 0.045) + "px) rotate(-11deg)";
+        ghost.style.transform = "translateY(" + (window.scrollY * 0.045) + "px) rotate(-28deg)";
         ticking = false;
       });
     }, { passive: true });
